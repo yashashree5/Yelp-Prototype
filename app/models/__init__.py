59d4a37cfa9f351faa04266ee27e765ba04d1a01
AndrewC04 <andrew20chau@gmail.com>
2026-03-06 18:23:56 -0800

    Restructuring repo

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.restaurant import Restaurant
-from app.models.review import Review
-from app.models.favorite import Favorite
-from app.models.preferences import UserPreferences
-from app.models.owner import RestaurantOwner
